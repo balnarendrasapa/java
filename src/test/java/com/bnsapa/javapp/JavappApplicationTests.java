13927f364956e680b6249b2b79ebd90223ce63a1
Bal Narendra Sapa <bnsapa2000@gmail.com>
2023-11-25 15:55:26 -0500

    Add JavappApplicationTests class

---
+++ src/test/java/com/bnsapa/javapp/JavappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnsapa.javapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
