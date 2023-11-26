d644823c0dc77b12a90cb2404fd8db0ce976af6e
Bal Narendra Sapa <bnsapa2000@gmail.com>
2023-11-25 21:46:36 -0500

    Commented out unnecessary code in ColoursApplication and added log statement in JavappApplication

--- src/main/java/com/bnsapa/javapp/ColoursApplication.java
+++ src/main/java/com/bnsapa/javapp/ColoursApplication.java
@@ -4,9 +4,9 @@ import com.bnsapa.javapp.services.ColourPrinter;
 import lombok.extern.java.Log;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+// @SpringBootApplication
 @Log
 public class ColoursApplication implements CommandLineRunner {
 
--- src/main/java/com/bnsapa/javapp/JavappApplication.java
+++ src/main/java/com/bnsapa/javapp/JavappApplication.java
@@ -1,10 +1,27 @@
 package com.bnsapa.javapp;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.bnsapa.javapp.services.ColourPrinter;
+
+import lombok.extern.java.Log;
+
 @SpringBootApplication
-public class JavappApplication {
+@Log
+public class JavappApplication implements CommandLineRunner {
+
+	private ColourPrinter colourPrinter;
+
+	public JavappApplication(ColourPrinter colourPrinter) {
+		this.colourPrinter = colourPrinter;
+	}
+
+	@Override
+	public void run(final String... args) {
+		log.info(colourPrinter.print());
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavappApplication.class, args);
